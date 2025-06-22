3a39b07ac1c88bb09b4e23df7ca2f42303905761
Mihret7 <mihretbizuayehu777@gmail.com>
2025-06-23 01:07:38 +0300

    deployment

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-9679c/us-central1/api",
+  baseURL: "https://amazon-api-f8sq.onrender.com",
 });
 
 export { axiosInstance };
